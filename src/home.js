d72835c15c9404a117d92dfacff1910eeeb76f7d
malki <malki79292@gmail.com>
2023-12-27 00:52:39 +0200

    basis

---
+++ src/home.js
@@ -0,0 +1,30 @@
+// import logo from "logo.png";
+function Entrance() {
+    return (
+        <button>כניסה</button>
+    );
+}
+function Enrollment() {
+    return (
+        <button>הרשמה</button>
+    );
+}
+function Home(){
+
+    return (
+        <div>
+            <button >הרשמה</button>
+            <button >כניסה</button>
+            <button >הצגת מתכונים</button>
+            {/* <img src={}/> */}
+        </div>
+    );
+}
+export default Home;
+// <form>
+//     <label>
+//         <p>שם המתכון</p>
+//     </label>
+//    //כאן צריל להיות המתכון וכל רכיב בו הוא כפתור ששולח לבלתי נודע
+//     <button></button>
+// </form>
